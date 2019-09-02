9aa0b629e1c33cd9d159734e39f430feb9212cdd
tnoyh <atnoyh@icloud.com>
2019-09-02 10:35:43 +0800

    新加了一个注册中心模块

---
+++ nevo-register/src/main/java/com/xtsz/register/NevoRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.xtsz.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevoRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NevoRegisterApplication.class, args);
+    }
+
+}
---
+++ nevo-register/src/test/java/com/xtsz/register/NevoRegisterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xtsz.register;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NevoRegisterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
